0feade2067d17c4f2b06eeb2f2b8c6c101764b87
apavlushina <ssemina7@gmail.com>
2020-03-04 20:29:23 +0100

    top news comp

--- src/components/TopNews/index.js
+++ src/components/TopNews/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getDescription } from "../../actions/actions";
 
-export default function TopNews() {
-  return <div>News will be there</div>;
+export class TopNews extends React.PureComponent {
+  state = {};
+
+  componentDidMount() {
+    console.log("props1", this.props);
+    // this.props.getDescription(this.state.stories, 5);
+  }
+
+  render() {
+    console.log("props2", this.props);
+    return <div>Some news there</div>;
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    detailedStories: state.detailedStories
+  };
+};
+
+export default connect(mapStateToProps, { getDescription })(TopNews);
